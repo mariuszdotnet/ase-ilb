f62391376f2f87089209c756f0b6897f5881788e
Natalia Konokhova <nataliak@microsoft.com>
2017-08-25 13:14:50 -0400

    Replaced parameteres to create resource ID for VNET. Commented out as ongoing work.

--- app-service-environment-v2/azuredeploy.ps1
+++ app-service-environment-v2/azuredeploy.ps1
@@ -1,17 +1,17 @@
 # Variables that need to be set
 
 # The subscriptionId
-$subscriptionId = '<SUBSCRIPTIONID>'
+$subscriptionId = '5cc5fbe7-51cc-4492-ad12-f923efbb9ade'
 # Root path to script, template and parameters.  All have to be in the same folder.
-$rootPath = '<PATHTOSCRIPTS>' # Replace with $PSScriptRoot if you want to run it as a script; EXAMPLE: $rootPath = 'C:\Users\makolo\Documents\GitHub\azure-vmss-templates\vm-simple-rhel'
+$rootPath = 'C:\Users\nataliak\Source\Repos\ase-ilb\app-service-environment-v2' # Replace with $PSScriptRoot if you want to run it as a script; EXAMPLE: $rootPath = 'C:\Users\makolo\Documents\GitHub\azure-vmss-templates\vm-simple-rhel'
 # Name of the resource group
-$resourceGroupName = '<RGNAME>'
+$resourceGroupName = 'ASEv2'
 # Resource Group Location 
-$resourceGroupLocation = '<RGLOCATIONS>' # Run <Get-AzureLocation> to find out azure locations; EXAMPLE: 'East US 2'
+$resourceGroupLocation = 'East US 2' # Run <Get-AzureLocation> to find out azure locations; EXAMPLE: 'East US 2'
 # Name of the deployment
-$deploymentName = '<DEPLOYMENTNAME>'
+$deploymentName = 'ASEv2'
 
-Login-AzureRmAccount
+#Login-AzureRmAccount
 
 Get-AzureSubscription
 
@@ -21,10 +21,10 @@ Select-AzureRmSubscription -SubscriptionId $subscriptionId
 New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
 
 # Run the below to test the ARM template
-Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json"
+#Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json" -TemplateParameterFile "$rootPath\azuredeploy.parameters.json"
 
 # Use parameter file
 New-AzureRmResourceGroupDeployment -verbose -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json" -TemplateParameterFile "$rootPath\azuredeploy.parameters.json"
 
 # Input parameters manually via CLI
-New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json"
+#New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json"
