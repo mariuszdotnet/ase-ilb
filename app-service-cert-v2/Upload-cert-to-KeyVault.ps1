c03908cb4ae670e1af547239f87a8b9ebaab600e
Natalia Konokhova <nataliak@microsoft.com>
2017-08-30 14:14:50 -0400

    Adding: ARM for WebApp, PS script for uploading the cert.

---
+++ app-service-cert-v2/Upload-cert-to-KeyVault.ps1
@@ -0,0 +1,19 @@
+﻿# Variables that need to be set
+
+# The subscriptionId
+$subscriptionId = '5cc5fbe7-51cc-4492-ad12-f923efbb9ade' #Subscription ID
+$KeyVaultName = "ASEv2-Keyvault"  #Key Vault Name
+$pfxFilePath = "PFX_CERTIFICATE_FILE_PATH" # Change this path 
+$pwd = "PFX_CERTIFICATE_PASSWORD" # Change this password 
+$KeyVaultSecretName="azureminilab_cert"     #change to set the name of the secret
+
+
+$flag = [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable 
+$collection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection 
+$collection.Import($pfxFilePath, $pwd, $flag) 
+$pkcs12ContentType = [System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12 
+$clearBytes = $collection.Export($pkcs12ContentType) 
+$fileContentEncoded = [System.Convert]::ToBase64String($clearBytes) 
+$secret = ConvertTo-SecureString -String $fileContentEncoded -AsPlainText –Force 
+$secretContentType = 'application/x-pkcs12' 
+Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name $KeyVaultSecretName -SecretValue $Secret -ContentType $secretContentType # Change Key Vault name and Secret name 
