6536ff36264fd850c55c0fc72cc50bec0391a0a3
Mariusz Kolodziej <makolo@microsoft.com>
2017-09-13 09:02:59 -0400

    cleaned up ase folder, created misc folder

--- app-service-cert-v2/Upload-cert-to-KeyVault.ps1
+++ app-service-cert-v2/Upload-cert-to-KeyVault.ps1
@@ -1,12 +1,17 @@
 ﻿# Variables that need to be set
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
 
 # The subscriptionId
-$subscriptionId = 'SUBSCRIPTION_ID' #Subscription ID
-$KeyVaultName = "KEY VAULT_NAME"  #Key Vault Name
-$pfxFilePath = "PFX_CERTIFICATE_FILE_PATH" # Change this path 
-$pwd = "PFX_CERTIFICATE_PASSWORD" # Change this password 
-$KeyVaultSecretName="CERTIFICATE_NAME"     #change to set the name of the secret
+$subscriptionId = "" #Subscription ID
+$KeyVaultName = ""  #Key Vault Name
+$pfxFilePath = "" # Change this path 
+$pwd = "" # Change this password 
+$KeyVaultSecretName=""     #change to set the name of the secret
 
+# One time action required per Azure Tenant to Authorize Azure.Web RP to access KeyVault
+# Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyVaultName -ServicePrincipalName abfa0a7c-a6b6-4736-8310-5855508787cd -PermissionsToSecrets get
 
 $flag = [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable 
 $collection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection 
