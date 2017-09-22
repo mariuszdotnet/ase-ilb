e3f4d28737d0c8d4576ce9df01d835991306debd
Natalia Konokhova <nataliak@microsoft.com>
2017-09-22 14:52:05 -0400

    multiple web app

--- app-service-web-app-v2/azuredeploy.ps1
+++ app-service-web-app-v2/azuredeploy.ps1
@@ -3,13 +3,13 @@
 # The subscriptionId
 $subscriptionId = '5cc5fbe7-51cc-4492-ad12-f923efbb9ade'
 # Root path to script, template and parameters.  All have to be in the same folder.
-$rootPath = 'C:\Users\makolo\git\ase-ilb\app-service-web-app-v2' # Replace with $PSScriptRoot if you want to run it as a script; EXAMPLE: $rootPath = 'C:\Users\makolo\Documents\GitHub\azure-vmss-templates\vm-simple-rhel'
+$rootPath = 'C:\Users\nataliak\Source\Repos\ase-ilb\app-service-web-app-v2' # Replace with $PSScriptRoot if you want to run it as a script; EXAMPLE: $rootPath = 'C:\Users\makolo\Documents\GitHub\azure-vmss-templates\vm-simple-rhel'
 # Name of the resource group
 $resourceGroupName = 'ASEv2'
 # Resource Group Location 
 $resourceGroupLocation = 'East US 2' # Run <Get-AzureLocation> to find out azure locations; EXAMPLE: 'East US 2'
 # Name of the deployment
-$deploymentName = 'ASEv2web'
+$deploymentName = 'ASEv2webapp'
 
 #Login-AzureRmAccount
 
@@ -18,7 +18,7 @@ Get-AzureSubscription
 Select-AzureRmSubscription -SubscriptionId $subscriptionId
 
 # Create the new Azure Resource Group
-New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Force
+#New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation -Force
 
 # Run the below to test the ARM template
 Test-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile "$rootPath\azuredeploy.json" -TemplateParameterFile "$rootPath\azuredeploy.parameters.json"
