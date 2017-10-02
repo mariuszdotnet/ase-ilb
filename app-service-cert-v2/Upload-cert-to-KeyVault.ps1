b328d952d3eec48057bf053c2341482107dcd42b
Mariusz Kolodziej <makolo@microsoft.com>
2017-10-02 09:35:34 -0400

    updates for ase cert

--- app-service-cert-v2/Upload-cert-to-KeyVault.ps1
+++ app-service-cert-v2/Upload-cert-to-KeyVault.ps1
@@ -1,24 +1,24 @@
-﻿# Variables that need to be set
-Login-AzureRmAccount
-Select-AzureRmSubscription -SubscriptionId $subscriptionId
+﻿#$subscriptionId = "5aec60e9-f535-4bd7-a951-2833f043e918" #Subscription ID
+$KeyVaultName = "ase-kv"  #Key Vault Name
+$KeyVaultSecretName="asecert2"     #change to set the name of the secret
 
-
-# The subscriptionId
-$subscriptionId = "" #Subscription ID
-$KeyVaultName = ""  #Key Vault Name
-$pfxFilePath = "" # Change this path 
-$pwd = "" # Change this password 
-$KeyVaultSecretName=""     #change to set the name of the secret
-
-# One time action required per Azure Tenant to Authorize Azure.Web RP to access KeyVault
-# Set-AzureRmKeyVaultAccessPolicy -VaultName $KeyVaultName -ServicePrincipalName abfa0a7c-a6b6-4736-8310-5855508787cd -PermissionsToSecrets get
-
-$flag = [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable 
+$pfxFilePath = 'C:\Users\makolo\git\ase-ilb\app-service-cert-v2\a-exportedcert.pfx'
+$pwd = 'password'
+$flag = [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable
 $collection = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2Collection 
-$collection.Import($pfxFilePath, $pwd, $flag) 
-$pkcs12ContentType = [System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12 
-$clearBytes = $collection.Export($pkcs12ContentType) 
-$fileContentEncoded = [System.Convert]::ToBase64String($clearBytes) 
-$secret = ConvertTo-SecureString -String $fileContentEncoded -AsPlainText –Force 
-$secretContentType = 'application/x-pkcs12' 
-Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name $KeyVaultSecretName -SecretValue $Secret -ContentType $secretContentType # Change Key Vault name and Secret name 
+$collection.Import($pfxFilePath, $pwd, $flag)
+#$pkcs12ContentType = [System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12
+$clearBytes = $collection.Export($pkcs12ContentType)
+$fileContentEncoded = [System.Convert]::ToBase64String($clearBytes)
+
+$fileContentEncoded
+
+#$secret = ConvertTo-SecureString -String $fileContentEncoded -AsPlainText –Force
+#$secretContentType = 'application/x-pkcs12'
+#Set-AzureKeyVaultSecret -VaultName $KeyVaultName -Name $KeyVaultSecretName -SecretValue $Secret -ContentType $secretContentType
+
+# simple version
+$fileName = "exportedcert.pfx"
+Export-PfxCertificate -cert $certThumbprint -FilePath $fileName -Password $password     
+$fileContentBytes = get-content -encoding byte $fileName
+$fileContentEncoded = [System.Convert]::ToBase64String($fileContentBytes)
---
+++ app-service-cert-v2/generate-export-cert.ps1
@@ -0,0 +1,16 @@
+﻿$certificate = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname "*.aseilb.com","*.scm.aseilb.com"
+
+$certThumbprint = "cert:\localMachine\my\" + $certificate.Thumbprint
+$password = ConvertTo-SecureString -String "password" -Force -AsPlainText
+
+$fileName = "a-exportedcert.pfx"
+Export-PfxCertificate -cert $certThumbprint -FilePath $fileName -Password $password     
+
+$fileContentBytes = get-content -encoding byte $fileName
+$fileContentEncoded = [System.Convert]::ToBase64String($fileContentBytes)
+$fileContentEncoded | set-content ($fileName + ".b64")
+
+$certThumbprint
+
+# Get All Thumbprints
+Get-ChildItem -path cert:\LocalMachine\My
